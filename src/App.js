2af9ce58e239987e483805dafa11087fd8e11f22
Eber Doe <eberg.pena@gmail.com>
2019-03-05 10:48:11 -0600

    Se agrego JSX para mejor funcionamiento de React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    //const nombre = 'Eber';
+
+    const empleado = {
+      nombre: 'Eber',
+      trabajo:'Desarrollador Web'
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+      <React.Fragment>
+      <h1 className= "mayusculas">Detalles de:</h1>  
+      <p>Nombre:{empleado.nombre}</p>
+      <p>Trabaja de:{empleado.trabajo}</p>
+      </React.Fragment>
     );
   }
 }
